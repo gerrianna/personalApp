e16425a3da48e234373a9efc6c276bd40144b265
gerrianna <gerri@brandeis.edu>
2016-06-09 14:08:24 -0400

    add review to comments section

--- pages/comments/client/comments.js
+++ pages/comments/client/comments.js
@@ -1,21 +1,40 @@
 Template.comments.helpers({
+	comments: function(){
+		return Comments.find({},{sort:{createdAt:-1}, limit:30});},
 
 })
 
 Template.comments.events({
-	"click .js-submit-comment": function(event){
+	"click .js-submit-comment": 
+	function(event){
 		event.preventDefault();
-		console.dir(event);//prints out event and lets you examin it
+		//console.dir(event);//prints out event and lets you examin it
 		const comment_text = $(".js-user-comment").val();
+		if(comment_text ==""){
+			window.alert("you must enter a comment");
+			return;
+		}
+		const comment_rating = $(".js-rating-comment").val();
 		const comment_obj =
 		{text: comment_text,
+			rating: comment_rating,
 			createdAt: new Date(),
 			createdBy: Meteor.userId(),
 			userEmail: Meteor.user().emails[0].address
-
 		};
-		console.dir(comment_obj);
-		//Router.go({{pathFor 'home'}});
+		//console.dir(comment_obj);
+		Comments.insert(comment_obj);
 		$(".js-user-comment").val("");
-	}
+		//Router.go('/');
+		//console.log("Did we get here???");
+	},
+})
+
+Template.commentRow.events({
+
+		"click .js-delete-comment": function(event){
+			console.log("clicked on the x");
+			Comments.remove(this._id);
+		},
+	
 })

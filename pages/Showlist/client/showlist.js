c96249cd23864b7304e97bbb531f6fd65dbeae0b
gerrianna <gerri@brandeis.edu>
2016-06-14 17:04:44 -0400

    changed show list

--- pages/Showlist/client/showlist.js
+++ pages/Showlist/client/showlist.js
@@ -1,5 +1,17 @@
 Template.showList.helpers({
 	items:function(){
-		return Items.find();
+		//const dest = $(" .js-dest").val();
+		return Items.find({item:"eggs"});
+	}
+})
+
+Template.showList.events({
+	"click .js-addItem": function(event){
+		console.log("added to shopping list");
+		const needToBuy = $(" .js-needToBuy").val();
+		const item =
+		{item:item}
+		console.dir(needToBuy);
+		
 	}
 })
